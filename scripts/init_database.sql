6d5a6e82c2ba01490a515a688c34a1bbfc5f6c4e
omarrexe <191442132+omarrexe@users.noreply.github.com>
2025-04-10 16:18:21 +0200

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,5 @@
+Script Purpose:
+This script creates a new database named 'DataWarehouse'. It first checks whether the database already exists; if it does, the database is dropped and then recreated. Afterward, the script sets up three schemas within the database: 'bronze', 'silver', and 'gold'.
+
+
+
