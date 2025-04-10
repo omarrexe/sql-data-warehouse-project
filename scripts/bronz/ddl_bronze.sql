8b2c7be8937bb53c1b474a87c1c001be2a3d0354
omarrexe <191442132+omarrexe@users.noreply.github.com>
2025-04-10 18:30:05 +0200

    Update ddl_bronze.sql

--- scripts/bronz/ddl_bronze.sql
+++ scripts/bronz/ddl_bronze.sql
@@ -45,13 +45,13 @@ IF OBJECT_ID('bronze.crm_prd_info', 'U') IS NOT NULL
 GO
 
  CREATE TABLE bronze.crm_prd_info (
-    prd_id              INT,
-    prd_key             NVARCHAR(50),
-    prd_nm				NVARCHAR(50),
-    prd_cost			 INT,
-	prd_line			NVARCHAR(50),
-	prd_start_dt		DATETIME,
-    prd_end_dt			DATETIME
+    prd_id INT,
+    prd_key NVARCHAR(50),
+    prd_nm NVARCHAR(50),
+    prd_cost INT,
+	prd_line NVARCHAR(50),
+	prd_start_dt DATETIME,
+	prd_end_dt DATETIME
 );
 
 GO
